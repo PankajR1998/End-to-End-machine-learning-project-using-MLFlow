b1f5d20508f341e9d57d2efe5c4c9682b86430df
Pankaj <rawatpankaj9919@gmail.com>
2023-08-05 12:30:54 +0530

    exception, logger

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md","r") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-End-machine-learning-project-using-MLFlow"
+AUTHOR_USER_NAME = "PankajR1998"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "rawatpankaj9919@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="a small python project for ml app.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_url={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+)
