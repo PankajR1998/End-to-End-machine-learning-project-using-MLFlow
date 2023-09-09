3a345d43105103f03a5c71f165db3fc1cd6a921d
Pankaj <rawatpankaj9919@gmail.com>
2023-09-09 16:51:48 +0530

    data Validation

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from mlproject import logger
 from mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
+from mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage 1"
 
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} Started. <<<<<<<<<<")
     obj = DataIngestionTrainingPipline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} Completed. <<<<<<<<<<")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage 2"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} Started. <<<<<<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} Completed. <<<<<<<<<<")
     
